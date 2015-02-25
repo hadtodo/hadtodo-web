cde483c4415ca9e8ce4170e433b462f5803ca15f
m4tx <m4tx@m4tx.pl>
2015-02-25 18:22:42 +0100

    Add "Add todo" action button

--- app/app.js
+++ app/app.js
@@ -1,8 +1,26 @@
 var app = angular.module('StarterApp', ['ngMaterial']);
 
-app.controller('AppController', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-    $scope.toggleSidenav = function(menuId) {
-        $mdSidenav(menuId).toggle();
-    };
+app.controller('AppController', ['$scope', '$mdSidenav', '$mdDialog',
+    function ($scope, $mdSidenav, $mdDialog) {
+        $scope.toggleSidenav = function (menuId) {
+            $mdSidenav(menuId).toggle();
+        };
+        $scope.addTodo = function (event) {
+            $mdDialog.show({
+                controller: AddTodoDialogController,
+                templateUrl: 'dialogs/addtodo.tmpl.html',
+                targetEvent: event
+            });
+        }
+    }]);
 
-}]);
+function AddTodoDialogController($scope, $mdDialog) {
+    $scope.cancel = function () {
+        $mdDialog.cancel();
+        console.log('Add todo dialog: cancel');
+    };
+    $scope.add = function () {
+        $mdDialog.hide();
+        console.log('Add todo dialog: add');
+    };
+}
